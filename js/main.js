55a0d73f41657d3568fbe213d47ebbef420587e9
cmenedes <cmenedes@yahoo.com>
2017-02-15 21:25:56 -0500

    basic gui display, each card has id associated with it. may need to redo logic to work with node.

--- js/main.js
+++ js/main.js
@@ -25,13 +25,16 @@ var numCards = 81;
 
 function Game(){
   this.deck = [];
+  var i = 0;
   this.generateDeck = function(){
     for(var s in shapes){
       for(var sh in shadings){
         for(var c in colors){
           for(var n in numbers){
             var url = "images/" + shapes[s] + "_" + colors[c] + "_" + shadings[sh] + "_" + numbers[n] + ".png";
-            this.deck.push(new Card(shapes[s], shadings[sh], colors[c], numbers[n],url));
+            var id = 'card' + i;
+            this.deck.push(new Card(shapes[s], shadings[sh], colors[c], numbers[n],url, id));
+            i++;
             //document.write("Shape: " + card.shape + "Shading: " + card.shading + "Number: " + card.number + "Color: " + card.color);
             //console.log("Shape: " + shapes[s] + "Shading: " + shadings[sh] + "Number: " + numbers[n] + "Color: " + colors[c]);
           }
@@ -40,12 +43,13 @@ function Game(){
     }
   };
 }
-function Card(shape, shading, color, number, image){
+function Card(shape, shading, color, number, image, id){
     this.shape = shape;
     this.shading = shading;
     this.number = number;
     this.color = color;
     this.imageSource = image;
+    this.id = id;
 }
 //+ Jonas Raoni Soares Silva
 //@ http://jsfromhell.com/array/shuffle [rev. #1]
@@ -59,12 +63,26 @@ var shuffle = function(v){
 function printDeck(deck){
   for(var card in deck){
     if(card < 9){   //display nine at a time(for now)
-      $(".cards").append("<img src='" + deck[card].imageSource + "'></img>")
+      //var x = "<img src='" + deck[card].imageSource + "' onclick = 'console.log('.')'></img>";
+      //var funct = "console.log(hello" +")";
+      //var funct = "console.log(" + '"hello"' + ")";
+      //var id1 = 'card' + card;
+      //console.log(id1);
+      var funct = "printID(\"" + deck[card].id + "\")";
+      var x = "<img src='" + deck[card].imageSource + "' onclick='"+ funct +"' id = '" + deck[card].id + "' </img>";
+      
+      $(".cards").append(x);
+      console.log(x);
       console.log("Card " + card + ": Shape: " + deck[card].shape + " Shading: " + deck[card].shading + " Number: " + deck[card].number + " Color: " + deck[card].color + "<br />");
     }
   }
 }
-
+function printID(id1){
+  var id = document.getElementById("" + id1 + "");
+  //console.log("" + id1 + "");
+  console.log(id);
+  console.log(id1);
+}
 function main(){
   var game = new Game();
   game.generateDeck();  //generate deck
