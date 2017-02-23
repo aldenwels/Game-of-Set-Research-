8107e5a2791051e36f64e72f718e6a817d45d08e
aldenwels <phil.gringer@gmail.com>
2017-02-23 14:11:02 -0500

    current state has issues to be fixed

--- js/game.js
+++ js/game.js
@@ -1,19 +1,21 @@
 /*     */
 
 var cardsPicked = [];
-
+var imgToChange = [];
+var randomNew = [];
+var setsPicked = [];
 
 
 $(document).ready(function() {
     console.log("GAME READY");
 });
 
-
 function printCurrentCards() {
     $(".cards > img").each(function() {
         console.log($(this).attr('id'));
     });
 }
+
 
 function cardPicked(id) {
   console.log(game);
@@ -22,9 +24,9 @@ function cardPicked(id) {
         console.log("Picked " + id);
 
         $("#" + id).toggleClass("shaded");
-        var obj = $.grep(game.deck, function(e){ return e['id'] == id; });
+        //card has its own ID change the grep function
+        var obj = $.grep(cardsDealt, function(e){ return e['id'] == id; });
         if(cardsPicked.includes(obj[0])){
-          console.log(id + " is in this bih");
           var index = cardsPicked.indexOf(id);
           cardsPicked.splice(index,1);
         }
@@ -49,8 +51,6 @@ var setCheck = {
   shading: []
 };
 
-//console.log(setCheck);
-
 
 //check the console for the results
 function checkIfSet(){
@@ -64,27 +64,34 @@ function checkIfSet(){
       }
     }
   }
-  console.log(setCheck);
   var isSet = true;
   for(var prop in setCheck){
     if(allValuesSame(setCheck[prop]) || unique(setCheck[prop])){
       console.log(prop + " satisfies");
-      removeSet();
     }
     else{
       console.log(prop + " does not satisfy");
       isSet = false;
-      removeSet();
     }
 
   }
-  if(isSet)
+  if(isSet){
     console.log("Cards selected are a set");
+    var holdIDs = [];
+    for(var i = 0; i < cardsPicked.length;i++)
+      holdIDs[i] = cardsPicked[i].id;
+    console.log(holdIDs);
+    replaceSet(holdIDs);
+    removeSet();
+    //remove in main
+  }
     //remove 3 cards from deck, add point to player, add 3 new cards to screen.
-  else
+  else{
     console.log("Cards selected are not a set");
-    //unshade the 3 cards, display message saying 'not a set'
+    removeSet();
+  }
 
+    //unshade the 3 cards, display message saying 'not a set'
 }
 
 //checks if all values in array are the same
@@ -114,4 +121,7 @@ function removeSet(){
     $("#"+cardsPicked[i].id).toggleClass("shaded");
   }
   cardsPicked = [];
+  for(var prop in setCheck){
+    setCheck[prop] = [];
+  }
 }
--- js/main.js
+++ js/main.js
@@ -7,52 +7,54 @@
     Multi-n
 */
 
-
+var cardsDealt = [];
 
 var shapes = [
-  "rectangle",
-  "circle",
-  "triangle"
+    "rectangle",
+    "circle",
+    "triangle"
 ];
 var shadings = [
-  "blank",
-  "striped",
-  "fill"
+    "blank",
+    "striped",
+    "fill"
 ];
 var colors = [
-  "blue",
-  "red",
-  "green"
+    "blue",
+    "red",
+    "green"
 ];
 var numbers = [
-  "1",
-  "2",
-  "3"
+    "1",
+    "2",
+    "3"
 ];
 var numCards = 81;
 
 
 
 var i = 1;
-function Game(){
-  this.deck = [];
-  this.generateDeck = function(){
-    for(var s in shapes){
-      for(var sh in shadings){
-        for(var c in colors){
-          for(var n in numbers){
-            var url = "images/" + shapes[s] + "_" + colors[c] + "_" + shadings[sh] + "_" + numbers[n] + ".png";
-            this.deck.push(new Card(shapes[s], shadings[sh], colors[c], numbers[n],url,i));
-            i++;
-            //document.write("Shape: " + card.shape + "Shading: " + card.shading + "Number: " + card.number + "Color: " + card.color);
-            //console.log("Shape: " + shapes[s] + "Shading: " + shadings[sh] + "Number: " + numbers[n] + "Color: " + colors[c]);
-          }
+
+function Game() {
+    this.deck = [];
+    this.generateDeck = function() {
+        for (var s in shapes) {
+            for (var sh in shadings) {
+                for (var c in colors) {
+                    for (var n in numbers) {
+                        var url = "images/" + shapes[s] + "_" + colors[c] + "_" + shadings[sh] + "_" + numbers[n] + ".png";
+                        this.deck.push(new Card(shapes[s], shadings[sh], colors[c], numbers[n], url, i));
+                        i++;
+                        //document.write("Shape: " + card.shape + "Shading: " + card.shading + "Number: " + card.number + "Color: " + card.color);
+                        //console.log("Shape: " + shapes[s] + "Shading: " + shadings[sh] + "Number: " + numbers[n] + "Color: " + colors[c]);
+                    }
+                }
+            }
         }
-      }
-    }
-  };
+    };
 }
-function Card(shape, shading, color, number, image,id){
+
+function Card(shape, shading, color, number, image, id) {
     this.shape = shape;
     this.shading = shading;
     this.number = number;
@@ -63,30 +65,60 @@ function Card(shape, shading, color, number, image,id){
 //+ Jonas Raoni Soares Silva
 //@ http://jsfromhell.com/array/shuffle [rev. #1]
 
-var shuffle = function(v){
-  for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+var shuffle = function(v) {
+    for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
     return v;
-  };
+};
 
 
-function printDeck(deck){
-  for(var card in deck){
-    if(card < 9){
-      //display nine at a time(for now)
-      $(".cards").append("<img src='" + deck[card].imageSource + "' id = '" + deck[card].id + "' </img>");
-      console.log("Card " + card + ": Shape: " + deck[card].shape + " Shading: " + deck[card].shading + " Number: " + deck[card].number + " Color: " + deck[card].color + "<br />");
+function printDeck() {
+    for (var card in deck) {
+        if (card < 9) {
+            //display nine at a time(for now)
+            $(".cards").append("<img src='" + deck[card].imageSource + "' id = '" + deck[card].id + "' </img>");
+            console.log("Card " + card + ": Shape: " + deck[card].shape + " Shading: " + deck[card].shading + " Number: " + deck[card].number + " Color: " + deck[card].color + "<br />");
+            cardsDealt.push(deck[card]);
+            //remove card from deck since is dealt
+            var rIndex = deck.indexOf(deck[card]);
+            console.log(rIndex);
+            deck.splice(rIndex,1);
+            console.log(deck);
+        }
     }
-  }
 }
+
+function replaceSet(ids) {
+    console.log("replacing");
+    var newSet = [];
+    for (var card in deck) {
+        if (newSet.length < 3) {
+            if (cardsDealt.includes(deck[card]) == false) {
+                newSet.push(deck[card]);
+                //remove dealt card from deck
+                var rIndex = deck.indexOf(deck[card]);
+                deck.splice(rIndex,1);
+            }
+        }
+    }
+    for (var i = 0; i < newSet.length; i++) {
+        $("#" + ids[i]).attr("id", newSet[i].id);
+        $("#" + newSet[i].id).attr('src', newSet[i].imageSource);
+        $("#" + newSet[i].id).toggleClass("shaded");
+    }
+}
+
 var game;
-function main(){
-  game = new Game();
-  game.generateDeck();  //generate deck
-  //printDeck(game.deck);
-  game.deck = shuffle(game.deck); //shuffle game deck
-  printDeck(game.deck); //print deck to screen
-  printCurrentCards();
-  $(".cards > img").click(function(){
-    cardPicked($(this).attr('id'));
-  });
+var deck;
+
+function main() {
+    game = new Game();
+    game.generateDeck(); //generate deck
+    //printDeck(game.deck);
+    game.deck = shuffle(game.deck); //shuffle game deck
+    deck = game.deck;
+    printDeck(game.deck); //print deck to screen
+    printCurrentCards();
+    $(".cards > img").click(function() {
+        cardPicked($(this).attr('id'));
+    });
 }
